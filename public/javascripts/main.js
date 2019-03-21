2e6c2609a607a2874662b055edf6674a42e774ed
santiago rodriguez matiz <s.rodriguezm@uniandes.edu.co>
2019-03-20 19:45:25 -0500

    pugs_main_io_modificados_falta

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,6 +11,58 @@ socket.on('messages', function(data){
   render(data, document.getElementById('sala').innerText);
 });
 
+function getProfileTemplate(profile){
+  var html = profile.map((elem, index)=>{
+    return(`<div>
+          <h1>Hola ${profile.firstname}!</h1>
+          <p>Te has logueado.</p>
+          <div class="form-group">
+            <button id="logout" class="btn btn-danger">Logout</button>
+          </div>
+       </div>`);
+  }).join(' ');
+}
+
+function profileController(){
+  document.getElemtById('logout')
+  .addEventListener('click', function(e){
+    e.preventDefault();
+    socket.emit('user.logout');
+    delete localStorage.token;
+    getRoute();
+  });
+}
+
+function getCreateAccountTemplate(){
+  var html = profile.map((elem, index)=>{
+    return(`<form id="create-account-form">
+              <h1>Crear cuenta</h1>
+              <div class="form-group">
+                <label>Nombre</label>
+                <input id="firstname" class="form-control"/>
+              </div>
+              <div class="form-group">
+                <label>Apellido</label>
+                <input id="lastname" class="form-control"/>
+              </div>
+              <div class="form-group">
+                <label>Email</label>
+                <input id="email" type="email" class="form-control"/>
+              </div>
+              <div class="form-group">
+                <label>Contraseña</label>
+                <input id="password" type="password" class="form-control"/>
+              </div>
+              <div class="form-group">
+                <button id="logout" class="btn btn-success">Crear Cuenta</button>
+                &nbsp; or &nbsp;
+                <a href="#/" class="btn btn-default">Login</a>
+              </div>
+              <div id="messages"></div>
+          </form>`);
+  }).join(' ');
+}
+
 function render(data){
     var html = data.map((elem,index)=>{
       return(`<div id="m">
